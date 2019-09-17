a0903e01f76c731b531f503ebde76d40ac22c265
shen11961677@163.com <shen11961677@163.com>
2019-09-17 17:24:53 +0800

    docs(doc): 修改启动脚本
    修改启动脚本

--- datax-web/script/start.sh
+++ datax-web/script/start.sh
@@ -1,3 +1,10 @@
 #!/bin/env bash
 
-java -Xmx1024m -Xms1024m -Xss256k -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=1024m -Ddatax.home=/data/application/datax -jar /data/application/datax/plugin/web/skycloud-datax-web-0.0.1-SNAPSHOT.jar &
+if [ -z "$1" ];then
+   echo "缺少环境变量参数，退出"
+   exit 1
+else
+   env="$1"
+fi
+
+java -Xmx1024m -Xms1024m -Xss256k -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=1024m -Denv=$env -Ddatax.home=/data/application/datax -jar /data/application/datax/plugin/web/skycloud-datax-web-0.0.1-SNAPSHOT.jar &
